cd386d65e08da4cba4b50d6019ea5da42707ae95
Hueu123 <chuhueu1@gmail.com>
2021-07-26 00:10:17 +0700

    lorem grocery navbar

---
+++ 11-navbar/src/components/Navbar.js
@@ -0,0 +1,56 @@
+import React, { useState, useRef, useEffect } from "react";
+import { FaBars } from "react-icons/fa";
+import { links, social } from "./data";
+import logo from "../logo.svg";
+const Navbar = () => {
+  const [showLink, setShowLink] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+  const toggleLinks = () => {
+    setShowLink(!showLink);
+  }
+  useEffect(() => {
+      const linkHeight = linksRef.current.getBoundingClientRect().height;
+      if(showLink){
+          linksContainerRef.current.style.height = `${linkHeight}px`;
+      }else{
+        linksContainerRef.current.style.height = "0px";
+      }
+  },[showLink]);
+  return (
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} alt="logo" className="logo" />
+          <button className="nav-toggle" onClick={() => toggleLinks}>
+            <FaBars />
+          </button>
+        </div>
+        <div className="links-container" ref={linksContainerRef}>
+          <ul className="links" ref={linksRef}>
+           {links.map((link) => {
+               const {id, url, text} = link;
+               return (
+                    <li key={id}>
+                        <a href={url}>{text}</a>
+                    </li>
+               )
+           })}
+          </ul>
+        </div>
+        <ul className="social-icons">
+            {social.map((item) => {
+                const {id, url , icon} = item;
+                return (
+                    <li key={id}>
+                        <a href={url}>{icon}</a>
+                    </li>
+                )
+            })}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
