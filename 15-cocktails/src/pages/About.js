00fc57987f3b8895f91540cd1c5ef5ba34ed02ae
Hueu123 <chuhueu1@gmail.com>
2021-08-05 17:07:23 +0700

    cocktails

---
+++ 15-cocktails/src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <section className="section about-section">
+            <h1 className="section-title">about us</h1>
+            <p>ReactJS offers graceful solutions to some of front-end programming’s most persistent issues, allowing you to build dynamic and interactive web apps with ease. It’s fast, scalable, flexible, powerful, and has a robust developer community that’s rapidly growing. There’s never been a better time to learn React.</p>
+        </section>
+    )
+}
+
+export default About
